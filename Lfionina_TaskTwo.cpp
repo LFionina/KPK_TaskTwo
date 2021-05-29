ac4712a3e37b694a687a34c16cafcf9745aee424
LFionina <80356955+LFionina@users.noreply.github.com>
2021-05-29 22:33:43 +0400

    двигаются два шарика.
    При столкновении двух шариков они меняют цвет и отскакивают друг от друга.

--- Lfionina_TaskTwo.cpp
+++ Lfionina_TaskTwo.cpp
@@ -13,9 +13,9 @@
 void Field (int indent, int borderSize, COLORREF colorBorder);
 void MoveBall (int indent,  int borderSize,
                int x = 300, int y = 100,
-               int vx = 7,  int vy = 3,
+               int vx = 9,  int vy = 3,
                int dt = 2);
-void Ball (int x, int y, int radius, const char text[]);
+void Ball (int x, int y, int radius, const char text[],int Red = 0, int Green = 0, int Blue = 0);
 
 
 //===========================================================================
@@ -45,43 +45,93 @@ void Field (int indent, int borderSize, COLORREF colorBorder)
 
 //-----------------------------------------------------------------------------
 void MoveBall (int indent,  int borderSize,
-               int x, int y,
+               int  x,  int  y,
                int vx,  int vy,
                int dt)
     {
 
-    int allIndent = indent + borderSize + 16;
+    int allIndentBall_1 = indent + borderSize + 16;
+    int allIndentBall_2 = indent + borderSize + 21;
+
+    int xBall_1  = x,        yBall_1  = y,       dtBall_1 = dt + 5, vxBall_1 =  vx, vyBall_1 =  vy;
+    int xBall_2  = x + 600,  yBall_2  = y + 650, dtBall_2 = dt,     vxBall_2 = -vx, vyBall_2 =  -vy;
 
     while (! txGetAsyncKeyState (VK_ESCAPE))
         {
-        Ball (x, y, 20, "1");
+        Ball (xBall_1, yBall_1, 20, "1", 10, x+20, 10);
+        Ball (xBall_2, yBall_2, 30, "2", y+20, 10, y+20);
+        printf("x= %d  y= %d\n", x, y);
 
-        x += vx * dt;
-        y += vy * dt;
+        xBall_1 += vxBall_1 * dtBall_1;
+        yBall_1 += vyBall_1 * dtBall_1;
 
-        if (x > 1200 - allIndent)
+        xBall_2 += vxBall_2 * dtBall_2;
+        yBall_2 += vyBall_2 * dtBall_2;
+
+        if (xBall_1 > 1200 - allIndentBall_1)
             {
-            vx = -vx;
-            x = 1200 - allIndent;
+            vxBall_1 = -vxBall_1;
+            xBall_1  = 1200 - allIndentBall_1;
             }
 
-        if (y >  700 - allIndent)
+        if (yBall_1 >  700 - allIndentBall_1)
             {
-            vy = -vy;
-            y =  700 - allIndent;
+            vyBall_1 = -vyBall_1;
+            yBall_1  = 700 - allIndentBall_1;
             }
 
-        if (x <    0 + allIndent)
+        if (xBall_1 <  0 + allIndentBall_1)
             {
-            vx = -vx;
-            x =    0 + allIndent;
+            vxBall_1 = -vxBall_1;
+            xBall_1  = 0 + allIndentBall_1;
             }
 
-        if (y <    0 + allIndent)
+        if (yBall_1 <  0 + allIndentBall_1)
             {
-            vy = -vy;
-            y =    0 + allIndent;
+            vyBall_1 = -vyBall_1;
+            yBall_1  = 0 + allIndentBall_1;
             }
+
+        //----------------
+        if (xBall_2 > 1200 - allIndentBall_2)
+            {
+            vxBall_2 = -vxBall_2;
+            xBall_2  = 1200 - allIndentBall_2;
+            }
+
+        if (yBall_2 >  700 - allIndentBall_2)
+            {
+            vyBall_2 = -vyBall_2;
+            yBall_2  = 700 - allIndentBall_2;
+            }
+
+        if (xBall_2 <  0 + allIndentBall_2)
+            {
+            vxBall_2 = -vxBall_2;
+            xBall_2  = 0 + allIndentBall_2;
+            }
+
+        if (yBall_2 <  0 + allIndentBall_2)
+            {
+            vyBall_2 = -vyBall_2;
+            yBall_2  = 0 + allIndentBall_2;
+            }
+
+        //----------------
+        int dR = (xBall_2 - xBall_1)*(xBall_2 - xBall_1) + (yBall_2 - yBall_1)*(yBall_2 - yBall_1);
+
+        if (dR <= 50*50)
+            {
+            vxBall_1 = -vxBall_1;
+            vyBall_1 = -vyBall_1;
+            x += 10;
+
+            vxBall_2 = -vxBall_2;
+            vyBall_2 = -vyBall_2;
+            y += 20;
+
+            }
+
 
         txSleep(100);
         Field (indent, borderSize, TX_ORANGE);
@@ -89,10 +139,10 @@ void MoveBall (int indent,  int borderSize,
 
     }
 //-----------------------------------------------------------------------------
-void Ball (int x, int y, int radius, const char text[])
+void Ball (int x, int y, int radius, const char text[], int Red, int Green, int Blue)
     {
     txSetColor (TX_LIGHTGREEN, 2);
-    txSetFillColor (TX_WHITE);
+    txSetFillColor (RGB(255 - Red, 255 - Green, 255 - Blue));
     txCircle (x, y, radius);
 
     txSelectFont ("Arial", radius, radius/4*3, true);
